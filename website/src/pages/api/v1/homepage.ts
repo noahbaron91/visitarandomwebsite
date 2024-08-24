a26ab63aae5ab8ab69d08261cd52ca2e74dbb39c
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-08-24 02:11:40 -0700

    feat: connect to d1

--- website/src/pages/api/v1/homepage.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = (ctx) => {
-  return new Response(
-    JSON.stringify({ success: true, url: 'https://amazon.com' }),
-    { status: 200 }
-  );
-};
--- website/src/pages/api/v1/page.ts
+++ website/src/pages/api/v1/page.ts
@@ -1,10 +1,15 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = (ctx) => {
+export const GET: APIRoute = async (ctx) => {
+  const db = ctx.locals.runtime.env.DB;
+  const statement = await db.prepare('SELECT * FROM page LIMIT 1');
+
+  const result = await statement.first();
+
   return new Response(
     JSON.stringify({
       success: true,
-      url: 'https://www.timeanddate.com/stopwatch/',
+      url: result.url,
     }),
     { status: 200 }
   );
