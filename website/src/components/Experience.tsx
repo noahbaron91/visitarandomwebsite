54c2909efcc9f658fc8017730b0a2239e5bdbe0b
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-08-30 04:46:47 -0700

    feat: text

--- website/src/components/Experience.tsx
+++ website/src/components/Experience.tsx
@@ -1,12 +1,29 @@
-import { useGLTF } from '@react-three/drei';
+import { Backdrop, Stage, useGLTF } from '@react-three/drei';
 
 export function Experience() {
   const model = useGLTF('/models/gumball_machine.glb');
 
+  console.log(model.animations);
+
+  const handleClick = (event: any) => {
+    console.log(event);
+  };
+
   return (
     <>
-      <ambientLight intensity={1} />
-      <primitive object={model.scene} scale={30} />
+      <Stage adjustCamera={true} />
+      {/* <primitive
+        object={model.scene}
+        scale={8}
+        position={[0, -2.5, 0]}
+        onClick={handleClick}
+      /> */}
+      {/* <Backdrop
+        floor={0.25} // Stretches the floor segment, 0.25 by default
+        segments={20} // Mesh-resolution, 20 by default
+      >
+        <meshStandardMaterial color='#ff80ae' />
+      </Backdrop> */}
     </>
   );
 }
---
+++ website/src/components/TextComponents.tsx
@@ -0,0 +1,10 @@
+export function TextComponents() {
+  return (
+    <p
+      style={{ top: 250, left: 100 }}
+      className='fixed text-gray-700 text-3xl hover:text-accent cursor-pointer'
+    >
+      thisisit.com
+    </p>
+  );
+}
